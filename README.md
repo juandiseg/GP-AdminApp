e134148c7438b5f79fa9d2585372f06cb67d7a82
juandiseg <juandiegosfrias@gmail.com>
2023-01-19 15:32:38 +0100

    test

--- README.md
+++
@@ -1,2 +0,0 @@
-# adminApp
-The administrator's desktop app
--- testing.java
+++
@@ -1,8 +0,0 @@
-public class testing {
-  int test = 1;
-
-  public static void main(String[] args) {
-    System.out.println("ola aqui estoy");
-  }
-
-}
