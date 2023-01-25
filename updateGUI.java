7f232244479a4051b676c35780b358118a9cc259
juandiseg <juandiegosfrias@gmail.com>
2023-01-25 10:15:13 +0100

    abstract class updateGUI

--- updateGUI.java
+++ updateGUI.java
@@ -1,7 +1,16 @@
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
 public abstract class updateGUI {
 
     private updateGUI previousWindow;
+    static JFrame theFrame;
     private int numberOfButtons;
+
+    updateGUI(JFrame theFrame, int numberOfButtons) {
+        this.theFrame = theFrame;
+        this.numberOfButtons = numberOfButtons;
+    }
 
     public abstract void addButtons();
 
@@ -17,7 +26,9 @@ public abstract class updateGUI {
     }
 
     private void refreshFrame() {
-
+        theFrame.invalidate();
+        theFrame.validate();
+        theFrame.repaint();
     }
 
 }
