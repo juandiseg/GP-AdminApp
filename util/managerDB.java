2dbc7b7eb1a357951245614b510d016319da3860
juandiseg <juandiegosfrias@gmail.com>
2023-01-30 00:28:03 +0100

    some comments about log-in

--- util/managerDB.java
+++ util/managerDB.java
@@ -18,8 +18,8 @@ import componentsFood.*;
 public class managerDB {
 
     private final static String url = "jdbc:mysql://localhost:3306/beatneat";
-    private final String user = "juandi";
-    private final String password = "Juandi";
+    private final String user = "juandi"; // Change to your local user
+    private final String password = "Juandi"; // Change to your local password
 
     public Connection getConnection() {
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
